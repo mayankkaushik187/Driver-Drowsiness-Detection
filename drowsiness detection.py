ab3ee56aff166ccd6554f15afb4926d60016e631
Mayank Kaushik <mayankkaushik187@gmail.com>
2021-11-09 22:02:11 +0530

    Added drowsiness files

---
+++ drowsiness detection.py
@@ -0,0 +1,15 @@
+import cv2
+import os
+from keras.models import load_model
+import numpy as np
+from pygame import mixer
+import time
+
+
+mixer.init()
+sound = mixer.Sound('alarm.wav')
+
+face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
+leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
+reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+
