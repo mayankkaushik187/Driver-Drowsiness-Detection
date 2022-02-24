28024f0f1cff1955d5e2420d1edf2ad697d8c5d5
Mayank Kaushik <mayankkaushik187@gmail.com>
2022-02-24 21:45:27 +0530

    Update drowsiness detection.py

--- drowsiness detection.py
+++ drowsiness detection.py
@@ -5,13 +5,15 @@ import numpy as np
 from pygame import mixer
 import time
 
-#initialising the mixer
+
 mixer.init()
 sound = mixer.Sound('alarm.wav')
 
 face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
 leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
 reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+
+
 
 lbl=['Close','Open']
 
